b43b63af74ee68e449a95f370794196d0a563b8f
elis-11 <eliza.arzanukaeva@gmail.com>
2022-07-10 19:37:50 +0200

    Refactoring

--- routes/users.router.js
+++ routes/users.router.js
@@ -1,36 +1,40 @@
-import express from "express"
+import express from "express";
 // import users from "../data/users.json"
-import {UserModel} from "../models/usersModel.js"
+import { UserModel } from "../models/usersModel.js";
 
 export const usersRouter = express.Router();
 
 // GET all users
-usersRouter.get("/", async(req, res) => {
-  const users = await UserModel.find()
+usersRouter.get("/", async (req, res) => {
+  const users = await UserModel.find();
   res.json(users);
 });
 
 // GET single
 usersRouter.get("/:id", async (req, res) => {
   const userId = req.params.id;
-const user = await UserModel.findById(userId);
+  const user = await UserModel.findById(userId);
   res.json(user);
 });
 
 // CREATE
-usersRouter.post("/", async (req, res)=>{
-  const userNew= await UserModel.create(req.body)
+usersRouter.post("/", async (req, res) => {
+  const userNew = await UserModel.create(req.body);
   res.json(userNew);
-})
+});
 
 // DELETE
-usersRouter.delete("/:id", async (req, res)=>{
-  const userDelete= await UserModel.findByIdAndDelete(req.params.id)
+usersRouter.delete("/:id", async (req, res) => {
+  const userDelete = await UserModel.findByIdAndDelete(req.params.id);
   res.json(userDelete);
-})
+});
 
 // EDIT
-usersRouter.patch("/:id", async (req, res)=>{
-const userUpdated= await UserModel.findByIdAndUpdate(req.params.id, req.body, {new: true})
-res.json(userUpdated);
-})
+usersRouter.patch("/:id", async (req, res) => {
+  const userUpdated = await UserModel.findByIdAndUpdate(
+    req.params.id,
+    req.body,
+    { new: true }
+  );
+  res.json(userUpdated);
+});
